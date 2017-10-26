21f5de2744367516fc4eccd17fca06b3502ce97b
Jakub Pelc <jakubspelc@gmail.com>
2017-10-26 19:47:30 +0200

    initial version of the project structure

---
+++ src/main/java/com/jpelc/budgetplanner/BudgetPlannerApplication.java
@@ -0,0 +1,12 @@
+package com.jpelc.budgetplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetPlannerApplication.class, args);
+	}
+}
